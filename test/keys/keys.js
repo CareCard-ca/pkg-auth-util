91f8856e9ce83fcb52abc8b810d88bc67606ff7f
Pankaj <pankajpriscilla@gmail.com>
2026-03-20 16:37:58 -0400

    Version 3.0.0 created.

---
+++ test/keys/keys.js
@@ -0,0 +1,13 @@
+const privateKey = "-----BEGIN PRIVATE KEY-----\n" +
+    "MC4CAQAwBQYDK2VwBCIEIIZb+iZmDglOb0b9WQAgujTcSkk4EHt9seE1fRaWTQKO\n" +
+    "-----END PRIVATE KEY-----";
+
+const publicKey = "-----BEGIN PUBLIC KEY-----\n" +
+    "MCowBQYDK2VwAyEACuMVU6yPYtjnwJwNnKPi5eBwTCmEi07ccNhrFqYadZo=\n" +
+    "-----END PUBLIC KEY-----";
+
+module.exports = exports = {
+    privateKey,
+    publicKey
+};
+
