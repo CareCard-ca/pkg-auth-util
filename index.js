d8c652b29ed5a0b083c03517cbaf7a5a7972c843
Pankaj <pankajpriscilla@gmail.com>
2026-05-09 01:24:19 -0400

    Coverage added.
    Signed-off-by: Pankaj <pankajpriscilla@gmail.com>

--- index.js
+++ index.js
@@ -3,33 +3,32 @@ const jwtUtilAuth = require('./lib/jwtUtilAuth');
 const keyGen = require('./lib/keyGen');
 
 module.exports = {
-    // New
-    generateKeyPair: keyGen.generateKeyPair,
-    jwtCreateSignedToken: jwtUtilAuth.createSignedJwtFromObject,
-    jwtVerifySignedToken: jwtUtilAuth.verifyJwtSignature,
-    jwtGetHeaderPayload: jwtUtilAuth.getHeaderPayloadFromJwt,
-    passwordCreateHashWithRandomSalt: pwdUtilAuth.createPasswordHashWithRandomSalt,
-    passwordCreateHashFromSavedHash: pwdUtilAuth.createPasswordHashBasedOnSavedAlgorithmSalt,
+  // New
+  generateKeyPair: keyGen.generateKeyPair,
+  jwtCreateSignedToken: jwtUtilAuth.createSignedJwtFromObject,
+  jwtVerifySignedToken: jwtUtilAuth.verifyJwtSignature,
+  jwtGetHeaderPayload: jwtUtilAuth.getHeaderPayloadFromJwt,
+  passwordCreateHashWithRandomSalt: pwdUtilAuth.createPasswordHashWithRandomSalt,
+  passwordCreateHashFromSavedHash: pwdUtilAuth.createPasswordHashBasedOnSavedAlgorithmSalt,
 
-    // Deprecated
-    /**
-     * @deprecated use direct import of the new functions.
-     */
-    jwtUtilAuth: {
-        createSignedJwtFromObject: jwtUtilAuth.createSignedJwtFromObject,
-        verifyJwtSignature: jwtUtilAuth.verifyJwtSignature,
-        getHeaderPayloadFromJwt: jwtUtilAuth.getHeaderPayloadFromJwt
-    },
-    /**
-     * @deprecated use direct import of the new functions.
-     */
-    pwdUtilAuth: {
-        createPasswordHashWithRandomSalt: pwdUtilAuth.createPasswordHashWithRandomSalt,
-        createPasswordHashBasedOnSavedAlgorithmSalt: pwdUtilAuth.createPasswordHashBasedOnSavedAlgorithmSalt
-    },
-    /**
-     * @deprecated Use native Buffer methods or other modern alternatives.
-     */
-    stringUtilAuth: require('./lib/stringUtilAuth'),
+  // Deprecated
+  /**
+   * @deprecated use direct import of the new functions.
+   */
+  jwtUtilAuth: {
+    createSignedJwtFromObject: jwtUtilAuth.createSignedJwtFromObject,
+    verifyJwtSignature: jwtUtilAuth.verifyJwtSignature,
+    getHeaderPayloadFromJwt: jwtUtilAuth.getHeaderPayloadFromJwt,
+  },
+  /**
+   * @deprecated use direct import of the new functions.
+   */
+  pwdUtilAuth: {
+    createPasswordHashWithRandomSalt: pwdUtilAuth.createPasswordHashWithRandomSalt,
+    createPasswordHashBasedOnSavedAlgorithmSalt: pwdUtilAuth.createPasswordHashBasedOnSavedAlgorithmSalt,
+  },
+  /**
+   * @deprecated Use native Buffer methods or other modern alternatives.
+   */
+  stringUtilAuth: require('./lib/stringUtilAuth'),
 };
-
