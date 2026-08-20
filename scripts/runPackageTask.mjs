482d8002e42d0d5d395a14ae65d540ec50a39302
Singh Pankaj K <8635636+singh-pankaj-k@users.noreply.github.com>
2026-08-20 13:26:56 -0400

    Test runner standardized. (#85)

--- scripts/runPackageTask.mjs
+++ scripts/runPackageTask.mjs
@@ -13,8 +13,20 @@ export const packageTasks = Object.freeze({
     { command: 'mocha', arguments: ['test/dependencyOverrides.audit.js'] },
   ],
   test: [
+    { command: 'npm', arguments: ['run', 'validate:audits'] },
     { command: 'npm', arguments: ['run', 'test:order'] },
-    { command: 'node', arguments: ['test/index.test.js'] },
+    { command: 'tsc', arguments: ['--noEmit'] },
+    {
+      command: 'mocha',
+      arguments: [
+        '--require',
+        './scripts/testOrder/randomizeTestOrder.cjs',
+        '-r',
+        'ts-node/register',
+        'test/types.test.ts',
+      ],
+    },
+    { command: 'nyc', arguments: ['node', 'test/index.test.js'] },
   ],
   'test:types': [
     { command: 'npm', arguments: ['run', 'test:order'] },
@@ -36,11 +48,7 @@ export const packageTasks = Object.freeze({
     { command: 'tsc', arguments: ['--noEmit'] },
     { command: 'nyc', arguments: ['node', 'test/index.test.js'] },
   ],
-  'test:All': [
-    { command: 'npm', arguments: ['run', 'validate:audits'] },
-    { command: 'npm', arguments: ['run', 'test'] },
-    { command: 'npm', arguments: ['run', 'test:types'] },
-  ],
+  'test:All': [{ command: 'npm', arguments: ['test'] }],
 });
 
 export function createTaskEnvironment(overrides = {}, inheritedEnvironment = process.env) {
